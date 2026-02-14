10095f311ec7c8452d30523baca814e83e39e82a
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-14 23:14:28 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <>
+        <App />
+        <Toaster closeButton={true} />
+    </>
 )
