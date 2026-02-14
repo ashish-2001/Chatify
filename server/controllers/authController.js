0784f1a6b3300b6efa451f541e589b6b6010e16e
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-14 23:38:16 +0530

    Added auth controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -6,7 +6,8 @@ const maxAge = 3 * 24 * 60 * 60 * 1000;
 
 const createToken = ( email, userId ) => {
     return jwt.sign({ email, userId }, process.env.JWT_KEY, { expiresIn: maxAge })
-}
+};
+
 async function signup(req, res){
 
     try{
@@ -51,6 +52,64 @@ async function signup(req, res){
     }
 }
 
+async function login(rq, res){
+
+    try{
+        const { email, password } = req.body;
+
+        if(!email || !password){
+            return res.status(400).json({
+                success: false,
+                message: "Email and password is required!"
+            });
+        }
+
+        const user = await User.findOne({
+            email
+        });
+
+        if(!user){
+            return res.status(404).json({
+                success: false,
+                message: "User not registered!"
+            });
+        };
+
+        const auth = await user.compare("password", user.password);
+
+        if(!auth){
+            return res.status(400).json({
+                success: false,
+                message: "Password is incorrect"
+            });
+        };
+
+        res.cookie("jwt", createToken(email, user.id), {
+            maxAge,
+            secure: true,
+            sameSite: "none"
+        });
+
+        return res.status(200).json({
+            success: true,
+            id: user.id,
+            email: user.email,
+            profileImage: user.profileImage,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            image: user.image,
+            color: user.color,
+            message: "Logged in successfully!"
+        });
+    } catch(e){
+        return res.status(500).json({
+            success: false,
+            message: e.message
+        });
+    };
+};
+
 export {
-    signup
+    signup,
+    login
 }
