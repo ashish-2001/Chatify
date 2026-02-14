649ea43c196a26687c1ca273390390239c52d009
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-14 23:43:38 +0530

    Added auth routes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,11 @@
 const HOST = import.meta.env.VITE_SERVER_URL;
 const AUTH_ROUTES = "api/auth";
 const SIGNUP_ROUTE = `${AUTH_ROUTES}/signup`;
-
+const LOGIN_ROUTE = `${AUTH_ROUTES}/login`;
 
 export {
     HOST,
     SIGNUP_ROUTE,
+    LOGIN_ROUTE,
     AUTH_ROUTES
 };
